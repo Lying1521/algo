7d4b71c74dd44faf266cf37b85d434501db4cf0f
Light <li_yu@pku.edu.cn>
2023-10-23 11:54:52 +0800

    leetcode 12.整数转罗马数字

--- List/数字转罗马数字.py
+++ List/数字转罗马数字.py
@@ -18,4 +18,49 @@
 # C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 # 给你一个整数，将其转为罗马数字。
 
-# 思路
+# 思路 贪心每次取最大
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        res = ""
+        while num > 0:
+            if num >= 1000:
+                num -= 1000
+                res += "M"
+            elif num >= 900:
+                num -= 900
+                res += "CM"
+            elif num >= 500:
+                num -=500
+                res += "D"
+            elif num >= 400:
+                num -= 400
+                res += "CD"
+            elif num >= 100:
+                num -= 100
+                res += "C"
+            elif num >= 90:
+                num -= 90
+                res += "XC"
+            elif num >= 50:
+                num -=50
+                res += "L"
+            elif num >= 40:
+                num -= 40
+                res += "XL"
+            elif num >= 10:
+                num -= 10
+                res += "X"
+            elif num >= 9:
+                num -= 9
+                res += "IX"
+            elif num >= 5:
+                num -=5
+                res += "V"
+            elif num >= 4:
+                num -= 4
+                res += "IV"
+            elif num >= 1:
+                num -= 1
+                res += "I"
+        return res
